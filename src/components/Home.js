a91507a8469e35b4071c365401ed783e4a847488
Christian R. Lara <christianramirezlara1@gmail.com>
2023-06-05 11:12:43 -0600

    Prepare project with SPA created.

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+export const Home = (onNavigate) => {
+  const HomeDiv = document.createElement('div');
+  const buttonLogin = document.createElement('button');
+
+  buttonLogin.textContent = 'Inicia sesión';
+
+  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+
+  HomeDiv.appendChild(buttonLogin);
+
+  return HomeDiv;
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
-// Este es el punto de entrada de tu aplicacion
+import { Home } from './components/Home.js';
+import { Login } from './components/Login.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
 
-myFunction();
+const routes = {
+  '/': Home,
+  '/login': Login,
+};
+
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+
+  rootDiv.appendChild(routes[pathname](onNavigate));
+};
+
+const component = routes[window.location.pathname];
+
+window.onpopstate = () => {
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+  rootDiv.appendChild(routes[window.location.pathname](onNavigate));
+};
+
+rootDiv.appendChild(component(onNavigate));
