b55f347a46049a1d8c333446b46383d40ff1adba
piaacv <122986202+piaacv@users.noreply.github.com>
2023-06-23 11:36:51 -0400

    funcionalidades

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,8 @@
 // aqui exportaras las funciones que necesites
 
-import { addDoc, collection, getDocs, doc, deleteDoc} from "@firebase/firestore";
+import { addDoc, collection, getDocs, doc, deleteDoc,} from "@firebase/firestore";
 import { auth, db } from "../firebase";
+import { signOut } from 'firebase/auth';
 
 export const myFunction = () => {
   // aqui tu codigo
@@ -11,6 +12,7 @@ export const myFunction = () => {
 export const createNewPost = (contentNewPost, usuario) => {
    return addDoc(collection(db, "post"), {
     contenido: contentNewPost,
+    userName: auth.currentUser.displayName,
     usuario
   });
 };
@@ -19,25 +21,22 @@ export const getAllPost = () => {
   return getDocs(collection(db, "post"));
 };
 
+export const LogOut = () => signOut(auth);
 
-
-// export const deletePost = async (contentNewPost) => {
-//   const option = confirm('Confirma para borrar tu post');
-//     if (option === true) {
-//       await deleteDoc(doc(db, 'post'));
+export const deletePost = async (id) => {
+  const confirmDelete = confirm('Confirma para borrar tu post');
+    if (confirmDelete === true) {
+      await deleteDoc(doc(db, 'post', id));
       
-//     } else {
-//       onNavigate('/login');
-//     } }
-// ;
+    } }
+;
 
 
 
-//crear función borrar post
-//crear función obtener post
+
+
 //crear función editar post
 //crear función darle like
-//iniciar sesión con google?
 //crear función dejar comentario
 //crear función editar nombre
 //crear función editar foto perfil
